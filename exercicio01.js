f4bf1f0628b25c34f3d3c906ea812061b50c250c
Bruna Savelli <bnsavelli@gmail.com>
2024-08-15 09:32:56 -0300

    exercicio

--- exercicio01.js
+++ exercicio01.js
@@ -1,38 +1,39 @@
 const livro = new Object();
-livro.nome = "Harry Potter";
-livro.autor = "J. K. Rowling";
-livro.ano = 1997;
-console.log(livro);
+livro.nome = "Harry Potter"
+livro.autor = "J. K. Rowling"
+livro.ano = 1997
+console.log(livro)
 
-const aluno = new Object();
-aluno.nome = "Bruna";
-aluno.turma = "2TDS1";
-aluno.idade = 16;
-console.log(aluno);
+const aluno = new Object()
+aluno.nome = "Bruna"
+aluno.turma = "2TDS1"
+aluno.idade = 16
+console.log(aluno)
 
-const carro = new Object();
-carro.cor = "Preto";
-carro.modelo = "gol GTI";
-carro.ano = 2024;
+const carro = new Object()
+carro.cor = "Preto"
+carro.modelo = "gol GTI"
+carro.ano = 2024
+console.log(carro)
 
-carro.modelo = "Nissan GT-R";
-carro.ano = 2022;
-console.log(carro);
+carro.modelo = "Nissan GT-R"
+carro.ano = 2022
+console.log(carro)
 
-const filme = new Object();
-filme.titulo = "O Diário da Princesa";
-filme.diretor = "Garry Marshall";
-filme.anoLancamento = 2002;
-console.log(filme);
+const filme = new Object()
+filme.titulo = "O Diário da Princesa"
+filme.diretor = "Garry Marshall"
+filme.anoLancamento = 2002
+console.log(filme)
 
-const cachorro = new Object();
-cachorro.nome = "Mini";
-cachorro.raca = "Salsicha";
-cachorro.cor = "Caramelo";
-console.log(cachorro);
+const cachorro = new Object()
+cachorro.nome = "Mini"
+cachorro.raca = "Salsicha"
+cachorro.cor = "Caramelo"
+console.log(cachorro)
 
-const produto = new Object();
-produto.descricao = "Caneca que muda de cor de acordo com a temperatura do líquido";
-produto.marca = "Stanley";
-produto.preço = 150;
-console.log(produto);
+const produto = new Object()
+produto.descricao = "Caneca que muda de cor de acordo com a temperatura do líquido"
+produto.marca = "Stanley"
+produto.preço = 150
+console.log(produto)
