c061ff3cb5ca4d96dba68a50615d678577e0d22f
Bruna Savelli <bnsavelli@gmail.com>
2024-08-15 08:53:52 -0300

    exemplos

---
+++ aula04.js
@@ -0,0 +1,13 @@
+let pessoa = "Bruna";
+
+const pessoaObjeto = {
+    corCabelo: "Castanho",
+    corOlhos: "Preto",
+    corPele: "Morena",
+    idade: 16,
+    altura: 1.61,
+    peso: 56
+}
+
+console.log(pessoa);
+console.log(pessoaObjeto);
---
+++ exercicio01.js
@@ -0,0 +1,35 @@
+const livro = new Object();
+livro.nome = "Harry Potter";
+livro.autor = "J. K. Rowling";
+livro.ano = 1997;
+console.log(livro);
+
+const aluno = new Object();
+aluno.nome = "Bruna";
+aluno.turma = "2TDS1";
+aluno.idade = 16;
+console.log(aluno);
+
+const carro = new Object();
+carro.cor = "Preto";
+carro.modelo = "gol GTI";
+carro.ano = 2024;
+console.log(carro);
+
+const filme = new Object();
+filme.titulo = "O Diário da Princesa";
+filme.diretor = "Garry Marshall";
+filme.anoLancamento = 2002;
+console.log(filme);
+
+const cachorro = new Object();
+cachorro.nome = "Mini";
+cachorro.raca = "Salsicha";
+cachorro.cor = "Caramelo";
+console.log(cachorro);
+
+const produto = new Object();
+produto.descricao = "Caneca que muda de cor de acordo com a temperatura do líquido";
+produto.marca = "Stanley";
+produto.preço = 150;
+console.log(produto);
